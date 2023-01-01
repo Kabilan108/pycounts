6363bad826721f68da136f153e5b0f80b13a317f
Tony Okeke <tonykabilanokeke@gmail.com>
2023-01-01 16:51:04 -0500

    Add test for plot_words

--- tests/test_pycounts.py
+++ tests/test_pycounts.py
@@ -1,5 +1,6 @@
-from pycounts_k108 import count_words
+from pycounts_k108 import count_words, plot_words
 from collections import Counter
+from matplotlib import container
 
 def test_count_words():
     """Test word counting from a file."""
@@ -9,3 +10,15 @@ def test_count_words():
                         'different': 1, 'results': 1})
     actual = count_words("tests/einstein.txt")
     assert actual == expected, "Einstein quote counted incorrectly!"
+
+def test_plot_words():
+    """Test plotting of word counts"""
+    counts = Counter({'insanity': 1, 'is': 1, 'doing': 1, 
+                      'the': 1, 'same': 1, 'thing': 1, 
+                      'over': 2, 'and': 2, 'expecting': 1,
+                      'different': 1, 'results': 1})
+    fig = plot_words(counts)
+    assert isinstance(fig, container.BarContainer), \
+           "Wrong plot type"
+    assert (len(fig.datavalues) == 10,  # type: ignore
+           "Incorrect number of bars plotted")
